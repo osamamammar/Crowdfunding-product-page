96166f4092dfac129c33c48a961ab78f5e8eac31
osamamammar <osamaammar29@gmail.com>
2021-10-07 08:21:36 +0200

    solve challenge with first mobile approch

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// MOBILE MENU
+// const toggleMenu = document.getElementById("toggle-menu");
+// const mobileMenu = document.querySelector(".mobile-menu--close");
+// const menu = document.getElementById("myLinks");
+
+// toggleMenu.addEventListener("click", function () {
+//   this.classList.toggle("hidden");
+//   mobileMenu.classList.toggle("hidden");
+//   menu.style.display = "flex";
+//   menu.style.flexDirection = "column";
+//   menu.style.justifyContent = "center";
+//   menu.style.alignItems = "center";
+//   menu.style.gap = "rem";
+// });
+// mobileMenu.addEventListener("click", function () {
+//   this.classList.toggle("hidden");
+//   toggleMenu.classList.remove("hidden");
+//   menu.style.display = "none";
+// });
