e9093a68c0f5e601c48e1029885cbeb8d6d37799
osamamammar <osamaammar29@gmail.com>
2021-10-11 01:18:32 +0200

    :hammer: :boom: fix some error in ui and add hamburger menu for small screens

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 // MOBILE MENU
+const overlay = document.querySelector(".overlay");
+const burgerIcon = document.querySelector(".burger-icon");
+const navList = document.querySelector(".nav-list");
+
+function toggleMenu() {
+  overlay.classList.toggle("active");
+  burgerIcon.classList.toggle("open");
+  navList.classList.toggle("open");
+}
+
+burgerIcon.addEventListener("click", toggleMenu);
+overlay.addEventListener("click", toggleMenu);
